bfb293ca5c29d697a14260c9592be03e962193a1
Zhao Li <yongle1360@gmail.com>
2026-05-28 15:31:57 -0400

    grid

---
+++ debug/debug/android/app/src/main/kotlin/com/example/debug/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.debug
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- list/lst/lib/main.dart
+++ list/lst/lib/main.dart
@@ -1,42 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(  ));
 }
 
 
 class MyApp extends StatelessWidget{
+  MyApp():super();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'jspang demos2',
+      title: 'jspang demos24',
       home: Scaffold(
         appBar: AppBar(title: Text('ListView')),
-        body: Center(
-          child: Container(
-            height: 200.0,
-            child: MyList()
-          )
+        body: GridView.count(
+          padding: const EdgeInsets.all(20.0),
+          crossAxisSpacing: 10.0, 
+          crossAxisCount: 3,
+          children: [
+            const Text('data 1'), 
+            const Text('data 2' ), 
+            const Text('data 3'), 
+            const Text('data 4'), 
+            const Text('data 5'), 
+            const Text('data 6'), 
+            const Text('data 7'),
+            const Text('data a') 
+            ],
         )
-
       ),
     );
   }
 
 }
 
-class MyList extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                Container(width: 280, color: Colors.lightBlue),
-                Container(width: 280, color: Colors.yellow),
-                Container(width: 180, color: Colors.deepOrange),
-                Container(width: 180, color: Colors.deepPurple),
-              ],
-            );
-  }
-}
+
+
+
