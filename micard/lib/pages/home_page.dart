2ae3730877c3e57dad53e80a94fc68a9e31a4cb6
Zhao Li <yongle1360@gmail.com>
2026-05-29 14:31:07 -0400

    list

--- micard/lib/pages/home_page.dart
+++ micard/lib/pages/home_page.dart
@@ -36,7 +36,7 @@ class _HomePageState extends State<HomePage> {
             Expanded(
                child: ListView.builder(
                 shrinkWrap: true,
-                itemCount: 10,
+                itemCount: 50,
                 itemBuilder: (context, index) {
                   return  _listItemView();
                 },
@@ -49,31 +49,41 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _listItemView() { 
-    return Column(
-      children: [
-        //the first row, avatar, author, date, top4
-        Row(children: [
-
-          Image.network('https://plus.unsplash.com/premium_photo-1666672388644-2d99f3feb9f1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          width: 30,           height: 30,           fit: BoxFit.cover, ),
-          Text('Author', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),),
-          Expanded(child: SizedBox()),
-          Text('2026-01-01', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-          Text('Top4', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),)
-        ],),
-        //the second row, title
-        Text('3TitleTitleTitleTitleTitleTitleTitleTitleTitle'),
-        //the third row, content picture
-        Row(
-          children: [
-            Text('Content'),
+    return Container(
+      margin: EdgeInsets.all( 5),
+      padding: EdgeInsets.all( 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        // border: Border(bottom: BorderSide(color: Colors.grey, width: 0.5))
+        border: Border.all(color: Colors.black12, width: 0.5),
+        borderRadius: BorderRadius.circular(15)
+      ),
+      child: Column(
+        children: [
+          //the first row, avatar, author, date, top4
+          Row(children: [
+      
+            Image.network('https://plus.unsplash.com/premium_photo-1666672388644-2d99f3feb9f1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            width: 30,           height: 30,           fit: BoxFit.cover, ),
+            Text('Author', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),),
             Expanded(child: SizedBox()),
-            Image.asset('assets/images/ic_launcher.png', width: 30, height: 30, fit: BoxFit.cover,),
-          ],
-        
-        )
-      ]
-
+            Text('2026-01-01', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+            Text('Top4', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),)
+          ],),
+          //the second row, title
+          Text('3TitleTitleTitleTitleTitleTitleTitleTitleTitle'),
+          //the third row, content picture
+          Row(
+            children: [
+              Text('Content'),
+              Expanded(child: SizedBox()),
+              Image.asset('assets/images/ic_launcher.png', width: 30, height: 30, fit: BoxFit.cover,),
+            ],
+          
+          )
+        ]
+      
+      ),
     );
   }
 
