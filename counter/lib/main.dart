b39ee04966d35ec1aec104f572932a1462a44f6f
Zhao Li <yongle1360@gmail.com>
2026-05-30 19:39:08 -0400

    nav

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -33,16 +33,45 @@ class HomePage extends StatefulWidget {
 class  _HomePageState extends State<HomePage> {
 
   final _words = <WordPair>[];
+  final _myWords = <WordPair>[];
+  Icon icon= Icon( Icons.favorite_border);
 
   @override
   Widget build(BuildContext context) {
     _words.addAll(generateWordPairs().take(10));
     return Scaffold(
         appBar: AppBar(
-          title: const Text('aixinpeidui1', textAlign: TextAlign.center),
+          title: const Text('aixinpeidui23', textAlign: TextAlign.center),
+          actions: [
+            IconButton(
+              onPressed: _goToLove,
+              icon: const Icon(Icons.list),
+            )
+          ],
         ),
         body: _buildList(),
       );
+  }
+
+  void _goToLove() {
+
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => LovePage(myWords: _myWords)));
+              // print(_myWords.length);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(title: const Text('我喜欢的单词')),
+          body: ListView.builder(
+            itemCount: _myWords.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text("$index:- ${_myWords[index].asCamelCase}"),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _buildList() {
@@ -61,9 +90,36 @@ class  _HomePageState extends State<HomePage> {
   }
 
   ListTile _buildRow(int index) {
+    WordPair word = _words[index];
     return ListTile(
-        title: Text( _words[index].asCamelCase ),
+        title: Text( word.asCamelCase ),
+        trailing: setIcon(word),
+        onTap: () {
+
+            _addToLove(word);
+            // print(_myWords);
+
+        },
       );
+  }
+
+
+  void _addToLove(WordPair word) {
+    setState(() {
+      if (_myWords.contains(word)) {
+        _myWords.remove(word);
+      } else {
+        _myWords.add(word);
+      }
+    });
+  }
+  
+  Icon setIcon(WordPair word) {
+    if (_myWords.contains(word)) {
+      return const Icon(Icons.favorite, color: Colors.red);
+    } else {
+      return const Icon(Icons.favorite_border);
+    }
   }
 }
 
