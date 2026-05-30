86d7486076c78c529c089e8cae1a65b685b636aa
Zhao Li <yongle1360@gmail.com>
2026-05-29 21:00:19 -0400

    dev

--- micard/lib/pages/home_page.dart
+++ micard/lib/pages/home_page.dart
@@ -16,39 +16,43 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              color: Colors.white,
-              child: Swiper(
-                indicatorLayout: PageIndicatorLayout.COLOR,
-                autoplay: true,
-                pagination: const SwiperPagination(),
-                control: const SwiperControl(),
-                itemCount: 3,
-                itemBuilder: (context, index) {
-                  return Container(
-                    color: Colors.lightBlue,
-                    height: 150,
-                    margin: EdgeInsets.all(0),
-                  );
-                },
-              ),
-            ),
-            Expanded(
-               child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: 50,
-                itemBuilder: (context, index) {
-                  return  _listItemView();
-                },
-              ),
-            )
-          ],
-        ),
+        child: banerList(),
       )
     );
+  }
+
+  Column banerList() {
+    return Column(
+        children: [
+          Container(
+            height: 200,
+            color: Colors.white,
+            child: Swiper(
+              indicatorLayout: PageIndicatorLayout.COLOR,
+              autoplay: true,
+              pagination: const SwiperPagination(),
+              control: const SwiperControl(),
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.lightBlue,
+                  height: 150,
+                  margin: EdgeInsets.all(0),
+                );
+              },
+            ),
+          ),
+          Expanded(
+             child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 50,
+              itemBuilder: (context, index) {
+                return  _listItemView();
+              },
+            ),
+          )
+        ],
+      );
   }
 
   Widget _listItemView() { 
@@ -103,15 +107,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   void onTapMain() {
-
-    Map<String, String> aug = { 'name': 'dark'};
-    // Navigator.pushNamed(context, RoutePath.webViewPage); 
-    RouteUtils.pushForNamed(context, RoutePath.webViewPage, arguments: aug);
-    /*
-    Navigator.push(
-      context, 
-      MaterialPageRoute(builder: (context) => WebViewPage(title: "from main", time: 'abc')));
-      */
+    RouteUtils.pushForNamed(context, RoutePath.webViewPage, arguments: { 'name': 'dark6'});
   }
 
 
