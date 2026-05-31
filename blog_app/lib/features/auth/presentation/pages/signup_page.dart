d57f718c395de90f461b7d963a76aa71a4f3f6ef
Zhao Li <yongle1360@gmail.com>
2026-05-31 03:03:07 -0400

    dev

--- blog_app/lib/features/auth/presentation/pages/signup_page.dart
+++ blog_app/lib/features/auth/presentation/pages/signup_page.dart
@@ -10,36 +10,74 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+
+  @override
+  dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // formKey.currentState?.validate();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Signup"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("2 "),
-            AuthField(
-              hintText: "Username",
-              controller: TextEditingController(),
-            ),
-            const SizedBox(height: 30),
-            AuthField(
-              hintText: "Email",
-              controller: TextEditingController(),
-            ),
-            const SizedBox(height: 30),
-            AuthField(
-              hintText: "Password",
-              controller: TextEditingController(),
-              isObscureText: true,
-            ),
-            const SizedBox(height: 15),
-            AuthGradientButton(buttonText: 'Signup', onPressed: (){})
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("5 "),
+              AuthField(
+                hintText: "Name",
+                controller: nameController,
+              ),
+              const SizedBox(height: 30),
+              AuthField(
+                hintText: "Email",
+                controller: emailController,
+              ),
+              const SizedBox(height: 30),
+              AuthField(
+                hintText: "Password",
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 15),
+              AuthGradientButton(buttonText: 'Signup', onPressed: (){}),
+          
+              RichText(
+                text: TextSpan(
+                  text: 'I\'am have an account? ',
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "Login",
+                      style: const TextStyle(
+                        color: Colors.blue,
+                      ),
+                      // recognizer: TapGestureRecognizer()..onTap = () {
+                      //   Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
+                      // },
+                    )
+                  ])
+              )
+              
+            ],
+          ),
         ),
       )
     );
