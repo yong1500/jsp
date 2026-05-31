2d3201510c720cd05ca7fd1167ed050ea61f42e7
Zhao Li <yongle1360@gmail.com>
2026-05-30 21:52:48 -0400

    structure

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,115 +10,20 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo3',
+      title: 'Flutter ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  // final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class  _HomePageState extends State<HomePage> {
-
-  final _words = <WordPair>[];
-  final _myWords = <WordPair>[];
-  Icon icon= Icon( Icons.favorite_border);
-
-  @override
-  Widget build(BuildContext context) {
-    _words.addAll(generateWordPairs().take(10));
-    return Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          title: const Text('aixinpeidui23', textAlign: TextAlign.center),
-          actions: [
-            IconButton(
-              onPressed: _goToLove,
-              icon: const Icon(Icons.list),
-            )
-          ],
+          title: const Text('Flutter 2'),
         ),
-        body: _buildList(),
-      );
-  }
-
-  void _goToLove() {
-
-              // Navigator.push(context, MaterialPageRoute(builder: (context) => LovePage(myWords: _myWords)));
-              // print(_myWords.length);
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => Scaffold(
-          appBar: AppBar(title: const Text('我喜欢的单词')),
-          body: ListView.builder(
-            itemCount: _myWords.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text("$index:- ${_myWords[index].asCamelCase}"),
-              );
-            },
-          ),
+        body: const Center(
+          child: Text('Hello, World!'),
         ),
       ),
     );
   }
-
-  Widget _buildList() {
-    return ListView.builder(
-      itemBuilder: (context, item) {
-
-        if (item.isOdd) return const Divider();
-        final index = item ~/ 2;
-        if (index >= _words.length) {
-          _words.addAll(generateWordPairs().take(10));
-        }
-
-        return _buildRow(index);
-      },
-    );
-  }
-
-  ListTile _buildRow(int index) {
-    WordPair word = _words[index];
-    return ListTile(
-        title: Text( word.asCamelCase ),
-        trailing: setIcon(word),
-        onTap: () {
-
-            _addToLove(word);
-            // print(_myWords);
-
-        },
-      );
-  }
-
-
-  void _addToLove(WordPair word) {
-    setState(() {
-      if (_myWords.contains(word)) {
-        _myWords.remove(word);
-      } else {
-        _myWords.add(word);
-      }
-    });
-  }
-  
-  Icon setIcon(WordPair word) {
-    if (_myWords.contains(word)) {
-      return const Icon(Icons.favorite, color: Colors.red);
-    } else {
-      return const Icon(Icons.favorite_border);
-    }
-  }
 }
+
 
--- counter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ counter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import shared_preferences_foundation
+import sqflite_darwin
+import url_launcher_macos
+import video_player_avfoundation
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
